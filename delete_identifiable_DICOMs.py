d4afca2f385b9ca798ec8d5b3977e19663e51179
jonasrichiardi <jonas.richiardi@chuv.ch>
2023-09-05 21:53:45 +0200

    refactor image type test; report number of files deleted; check if previous test already flagged file for deletion before doing more expensive tests

--- delete_identifiable_DICOMs.py
+++ delete_identifiable_DICOMs.py
@@ -10,13 +10,19 @@ from glob import glob
 import pydicom
 from tqdm import tqdm
 
+IMAGETYPES_TO_REMOVE=['SCREEN SAVE','DISPLAY','LOCALIZER','OTHER']
+
+
 def delete_identifiable_dicom_file(
-        filename: str) -> None:
+        filename: str) -> bool:
     """
     If identifiable data is present, deletes the Dicom file
 
     Args :
         filename: path to dicom image.
+
+    Returns:
+        bool: whether the file was deleted (True) or not (False)
 
     TODO:
         - Implement proper exception handling
@@ -33,38 +39,45 @@ def delete_identifiable_dicom_file(
 
     # parse DICOM header
     attributes = dataset.dir("")
-    if "ImageType" in attributes:
-        if 'SCREEN SAVE' in dataset.data_element('ImageType').value:
-            delete_this_file = True
-        if 'DISPLAY' in dataset.data_element('ImageType').value:
-            delete_this_file = True
-        if 'OTHER' in dataset.data_element('ImageType').value:
-            delete_this_file = True
-        if 'SECONDARY' in dataset.data_element('ImageType').value and 'CT' in dataset.data_element('Modality').value:
-            delete_this_file = True
-
     if "Modality" in attributes:
         if 'SR' in dataset.data_element('Modality').value:
             delete_this_file = True
 
-    if "ProtocolName" in attributes:
+    if not delete_this_file and ("ImageType" in attributes):
+        if any([this_type in dataset.data_element('ImageType').value for this_type in IMAGETYPES_TO_REMOVE]):
+            delete_this_file = True
+        if 'SECONDARY' in dataset.data_element('ImageType').value and 'CT' in dataset.data_element('Modality').value:
+            delete_this_file = True
+
+    if not delete_this_file and ("ProtocolName" in attributes):
         my_re_pn=re.compile('(?i).*(Scout|localizer|t2_haste_sag_ipat2).*')
         if my_re_pn.search(dataset.data_element('ProtocolName').value) is not None:
             delete_this_file = True
 
-    if "SeriesDescription" in attributes:
+    if not delete_this_file and ("SeriesDescription" in attributes):
         my_re_sd_morpho=re.compile('(?i).*(morpho|DEV).*')
         if my_re_sd_morpho.search(dataset.data_element('SeriesDescription').value) is not None:
             delete_this_file = True
-        my_re_sd_tof=re.compile('(?i).*tof.*')
-        if my_re_sd_tof.search(dataset.data_element('SeriesDescription').value) is not None:
-            delete_this_file = True
+        #my_re_sd_tof=re.compile('(?i).*tof.*')
+        #if my_re_sd_tof.search(dataset.data_element('SeriesDescription').value) is not None:
+        #    delete_this_file = True
         my_re_sd_report = re.compile('(?i).*report.*')
         if my_re_sd_report.search(dataset.data_element('SeriesDescription').value) is not None:
+            delete_this_file = True
+        my_re_sd_AAH = re.compile('(?i).*AAhead.*')
+        if my_re_sd_AAH.search(dataset.data_element('SeriesDescription').value) is not None:
+            delete_this_file = True
+        my_re_sd_rapid = re.compile('(?i).*rapid.*') # RAPID results
+        if my_re_sd_rapid.search(dataset.data_element('SeriesDescription').value) is not None:
+            delete_this_file = True
+        my_re_sd_Key = re.compile('(?i).*KEY_IMAGES.*') # Key images - potentially annotated
+        if my_re_sd_Key.search(dataset.data_element('SeriesDescription').value) is not None:
             delete_this_file = True
 
     if delete_this_file:
         os.remove(filename)
+
+    return delete_this_file
 
 
 def sanitize_all_dicoms_within_root_folder(
@@ -107,12 +120,19 @@ def sanitize_all_dicoms_within_root_folder(
                 # List all series dirs for this patient.
                 series_dirs = next(os.walk(os.path.join(datapath, patient, study_dir)))[1]
 
+                # Loop over this patient's series one by one
                 for series_dir in series_dirs:
                     all_filenames_series = glob(os.path.join(datapath, patient, study_dir,series_dir,'*'))
 
-                    # Loop over all dicom files within a patient directory and remove offending files
+                    # Loop over all dicom files within a series and remove offending files
+                    n_deleted_files_in_series = 0
                     for filename in all_filenames_series:
-                        delete_identifiable_dicom_file(filename)
+                        file_deleted = delete_identifiable_dicom_file(filename)
+                        if file_deleted:
+                            n_deleted_files_in_series += 1
+
+                    if n_deleted_files_in_series > 0:
+                        print(f"Deleted {n_deleted_files_in_series} files from series {series_dir}")
     return 0
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -130,6 +150,7 @@ def main(argv):
     print("Deleting potentially identifiable Dicom files within path {}".format(os.path.abspath(data_path)))
     # Sanitize all files.
     result = sanitize_all_dicoms_within_root_folder(datapath=data_path)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
